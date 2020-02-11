c70f12fec523e70f967d0ab1f517079638ccba6f
arl5705 <arl5705@rit.edu>
2020-02-11 13:47:04 -0500

    gutted parsing to use clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::env::args;
-use roogle::util::parse_args;
+extern crate clap;
+use clap::{Arg, App};
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    println!("{:?}", args);
-    let query: String = parse_args(args);
+    let matches = App::new("wiki")
+        .version("0.1.0")
+        .author("Aubrey Landau <aubreylandau@gmail.com>")
+        .about("get a wikipedia page")
+        .arg(Arg::with_name("query")
+            .required(true)
+            .takes_value(true)
+            .index(1)
+            .help("the article you want to find on wikipedia"))
+        .get_matches();
+    let query: &str = matches.value_of("query").unwrap();
 }
