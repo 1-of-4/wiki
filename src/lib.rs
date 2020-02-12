6d02e4bb080acc93e891f2339a6e587c683f554b
arl5705 <arl5705@rit.edu>
2020-02-11 20:03:43 -0500

    find_article() now returns vector containing names of similar articles to search

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub mod interface {
     use serde_json::Value;
     use std::io::Read;
 
-    pub fn find_article(query: &str) {
+    pub fn find_article(query: &str) -> Vec<String> { //todo: return a result
         let url: String = format!(
             "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&format=json",
             byte_serialize(query
@@ -18,6 +18,16 @@ pub mod interface {
         let mut response: String = String::new();
         get(&url).unwrap().read_to_string(&mut response);
         let results: Value = serde_json::json!(response);
+        let result_list: Vec<String> = results["query"]["search"]
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|v: &Value| v["title"]
+                .as_str()
+                .unwrap()
+                .to_owned()) //primary culprit if something goes wrong
+            .collect(); //todo: optimize this, jesus fuck
+        result_list
     }
 
     pub fn fetch_contents() {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use roogle::interface::find_article;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let app: App = App::from_yaml(yaml);
     let matches = app.get_matches();
     let query: &str = matches.value_of("query").unwrap();
+
+    let results: Vec<String> = find_article(query);
+    println!("{:?}", results)
 }
