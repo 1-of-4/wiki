455cbe8c8f8111a6d0922330dddf6877e1eddeef
arl5705 <arl5705@rit.edu>
2020-02-11 20:48:39 -0500

    abstracted out to Query and Request

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,44 @@ extern crate serde_json;
 pub mod interface {
     use reqwest::blocking::get;
     use url::form_urlencoded::byte_serialize;
-    use serde_json::Value;
+    use serde_json::{Value, json};
     use std::io::Read;
 
-    pub fn find_article(query: &str) -> Vec<String> { //todo: return a result
-        let url: String = format!(
-            "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&format=json",
-            byte_serialize(query
-                .as_bytes())
-                .collect::<String>() //convert to valid URL format (" " to "%20", for instance)
-        );
-        let mut response: String = String::new();
-        get(&url).unwrap().read_to_string(&mut response);
-        let results: Value = serde_json::json!(response);
-        let result_list: Vec<String> = results["query"]["search"]
+    enum Query {
+        search,
+        content,
+    }
+
+    struct Request {
+        query: Query,
+        keywords: String,
+    }
+
+    impl Request {
+        fn url(&self) -> String {
+            match &self.query {
+                Query::search => format!(
+                    "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&format=json",
+                    byte_serialize(self.keywords
+                        .as_bytes())
+                        .collect::<String>() //convert to valid URL format (" " to "%20", for instance)
+                    ),
+                Query::content => String::new(), //todo: content url
+            }
+        }
+
+        fn get_json(&self) -> Value {
+            let mut response: String = String::new();
+            get(&self.url())
+                .unwrap()
+                .read_to_string(&mut response);
+            return json!(response);
+        }
+    }
+
+    pub fn find_article(request: Request) -> Vec<String> { //todo: return a Result<Vector<String>, ?>
+        return request
+            .get_json()["query"]["search"]
             .as_array()
             .unwrap()
             .iter()
@@ -27,7 +51,6 @@ pub mod interface {
                 .unwrap()
                 .to_owned()) //primary culprit if something goes wrong
             .collect(); //todo: optimize this, jesus fuck
-        result_list
     }
 
     pub fn fetch_contents() {
