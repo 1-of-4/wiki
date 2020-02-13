db9094b5f93131cdf749f1fe30c6093ac592c58f
arl5705 <arl5705@rit.edu>
2020-02-13 13:37:24 -0500

    redid args layout to use subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #[macro_use]
 extern crate clap;
-use clap::App;
+use clap::{load_yaml, App};
 use roogle::wiki::*;
 
-fn main() { //todo: redo args to reflect new structure
+fn main() {
     let yaml = load_yaml!("cli.yml");
     let app: App = App::from_yaml(yaml);
     let matches = app.get_matches();
-    let query: &str = matches.value_of("query").unwrap();
+
+    //todo: parse args
 }
