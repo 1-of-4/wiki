ac3f366356fe5316e2c8d1b0823e9ed0d1241ae4
arl5705 <arl5705@rit.edu>
2020-02-14 00:29:40 -0500

    began frontend implementation of search() and made the snippet an Option<String> to reflect its optional nature

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate url;
 extern crate serde_json;
 
 pub mod wiki {
-    use reqwest::{blocking::get, Error};
+    use reqwest::blocking::get;
     use url::form_urlencoded::byte_serialize;
     use serde_json::{Value};
 
@@ -17,7 +17,6 @@ pub mod wiki {
 
     pub struct Request {
         query: Query,
-        keywords: String,
         url: String,
     }
 
@@ -34,7 +33,6 @@ pub mod wiki {
                 .replace("{}", keywords.as_ref()); //kinda jank but format! doesnt work with &str
             Request {
                 query,
-                keywords,
                 url
             }
         }
@@ -46,15 +44,24 @@ pub mod wiki {
             )
         }
 
-        pub fn search(&self) -> Safe<Vec<(String, String)>> {
-            let results = self.json().unwrap()["query"]["search"] //get from endpoint and navigate down to list of results
+        pub fn search(&self, snippet: bool) -> Safe<Vec<(String, Option<String>)>> { //todo: deal with html
+            let results = self.json()?["query"]["search"] //get from endpoint and navigate down to list of results
                 .as_array().unwrap() //convert to array
                 .iter()
                 .map(|result: &Value| {(
-                    result["title"].as_str().unwrap_or("No Title").to_string(),
-                    result["snippet"].as_str().unwrap_or("No Snippet").to_string()
-                    )}
-                )
+                    result["title"]
+                        .as_str()
+                        .unwrap_or("No Title")
+                        .to_string(),
+                    match snippet {
+                        true => Some(result["snippet"]
+                            .as_str()
+                            .unwrap_or("No Snippet")
+                            .to_string()
+                        ),
+                        false => None
+                    }
+                )})
                 .collect(); //shove all the elements into a nice little vector
             Ok(results)
         }
@@ -82,8 +89,7 @@ mod tests {
 
     #[test]
     fn search() {
-        let request = Request::new(Query::Search, "johnson").unwrap();
-        let results = request.search()?;
-        assert_eq!(results[0][0], "Johnson") //todo: deal with returned html
+        let results = Request::new(Query::Search, "ricardo").search(false).unwrap();
+        assert_eq!(results[1], (String::from("David Ricardo"), None));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-#[macro_use]
 extern crate clap;
 use clap::{load_yaml, App};
-use roogle::wiki::*;
+use wiki::wiki::*;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let app: App = App::from_yaml(yaml);
+    let app = App::from_yaml(yaml);
     let matches = app.get_matches();
 
-    //todo: parse args
+    match matches.subcommand_name() {
+        Some("search") => {
+            let request = Request::new(Query::Search, matches.value_of("keywords").unwrap());
+            let with_snippet = matches.is_present("snippet");
+            for (result, snippet) in request.search(with_snippet).unwrap() {
+                if let Some(i) = snippet {
+                    println!("{}\t{}", result, i)
+                } else {
+                    println!("{}", result)
+                }
+            }
+        },
+        _ => unimplemented!()
+    };
 }
