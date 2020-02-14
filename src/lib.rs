aa56e54bfed928c901c37b286f4ecfc52f8cc6a5
arl5705 <arl5705@rit.edu>
2020-02-13 19:17:22 -0500

    redid internal query structure, storing the url instead of automatically converting to json

--- src/lib.rs
+++ src/lib.rs
@@ -3,53 +3,67 @@ extern crate url;
 extern crate serde_json;
 
 pub mod wiki {
-    use reqwest::blocking::get;
+    use reqwest::{blocking::get, Error};
     use url::form_urlencoded::byte_serialize;
     use serde_json::{Value};
-    use std::error::Error;
-    type GenErr = Box<dyn Error> ;
+
+    type Safe<T> = Result<T, Box<dyn std::error::Error>>;
 
     pub enum Query {
         Search,
-        Content,
+        View,
+        Download,
     }
 
     pub struct Request {
         query: Query,
         keywords: String,
-        json: Value,
+        url: String,
     }
 
     impl Request {
-        pub fn new(query: Query, keywords: &str) -> Result<Request, GenErr> {
-            let keywords: String = byte_serialize(keywords.as_bytes())
+        pub fn new(query: Query, keywords: &str) -> Request {
+            let keywords = byte_serialize(keywords.as_bytes())
                 .collect::<String>(); //convert to valid URL format (" " to "+", for instance)
-            let url: String = match query {
-                Query::Search => format!("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&format=json", keywords),
-                Query::Content => format!("https://en.wikipedia.org/w/api.php?action=parse&page={}&prop=text&format=json", keywords),
-            };
-            let json = get(&url)?.json()?;
-            Ok(Request {
+            let url = match query {
+                Query::Search => "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&format=json",
+                Query::View => "https://en.wikipedia.org/w/api.php?action=parse&page={}&prop=text&format=json",
+                Query::Download => "https://en.wikipedia.org/w/index.php?title={}&action=raw"
+            }
+                .to_string()
+                .replace("{}", keywords.as_ref()); //kinda jank but format! doesnt work with &str
+            Request {
                 query,
                 keywords,
-                json,
-            })
+                url
+            }
         }
 
-        pub fn search(&self) -> Result<Vec<(String, String)>, GenErr> {
-            let results = self.json["query"]["search"] //get from endpoint and navigate down to list of results
+        fn json(&self) -> Safe<Value> {
+            Ok(
+                get(&self.url)?
+                    .json()?
+            )
+        }
+
+        pub fn search(&self) -> Safe<Vec<(String, String)>> {
+            let results = self.json().unwrap()["query"]["search"] //get from endpoint and navigate down to list of results
                 .as_array().unwrap() //convert to array
                 .iter()
                 .map(|result: &Value| {(
-                    result["title"].as_str().unwrap().to_string(),
-                    result["snippet"].as_str().unwrap().to_string()
+                    result["title"].as_str().unwrap_or("No Title").to_string(),
+                    result["snippet"].as_str().unwrap_or("No Snippet").to_string()
                     )}
                 )
                 .collect(); //shove all the elements into a nice little vector
             Ok(results)
         }
 
-        pub fn fetch(&self) -> Result<Vec<(String, Option<String>)>, GenErr> {
+        pub fn view(&self) -> Safe<String> {
+            unimplemented!()
+        }
+
+        pub fn download(&self) -> Safe<String> {
             unimplemented!()
         }
     }
@@ -70,6 +84,6 @@ mod tests {
     fn search() {
         let request = Request::new(Query::Search, "johnson").unwrap();
         let results = request.search()?;
-        assert_eq!(results[0], ("Johnson", r#"<span class=\"searchmatch\">Johnson</span> is a surname of English, Scottish origin. The name itself is a patronym of the given name John, literally meaning &quot;son of John&quot;. The name John"#))
+        assert_eq!(results[0][0], "Johnson") //todo: deal with returned html
     }
 }
